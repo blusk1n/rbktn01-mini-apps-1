a921643c7d6244c79a1f43c0f793d99a69861be5
Hamam-Elmuratdh <hamam.alsaiity@gmail.com>
2019-12-28 13:52:08 +0100

    s

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,31 @@
+const http = new XMLHttpRequest();
+document.getElementById("myform").addEventListener("click", function(event){
+    event.preventDefault()
+    const url = "/hello"
+    http.open("GET",url);
+    http.send()
+    http.onreadystatechange = function(){
+        if(this.readyState ===4 && this.status ===200){
+        let test = JSON.parse(http.responseText)
+            console.log(iterate(test))
+          document.getElementById("text").innerText += iterate(test)
+        
+        
+        }
+    }
+    
+  });
+  let str = ""
+  const iterate = (obj) => {
+    Object.keys(obj).forEach(key => {
 
+      str += key +" || "+ obj[key]+"\n"
 
+    if (Array.isArray(obj[key]) && obj[key].length !== 0 ) {
+            obj[key].forEach(elm=>{
+                iterate(elm)
+            })
+        }
+    })
+    return str
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const fs = require("fs")
+let app = express();
+let PORT =  5000;
+app.use(express.static('client'));
+app.listen(PORT, function() {
+  console.log(`listening on PORT ${PORT}`);
+});
 
+fs.readFile('./samples/sales_report.json', (err, data) => {
+    if (err) throw err;
+    let files = JSON.parse(data);
+    app.get("/hello",  (req, res) => {
+        res.send(files);
+      });
+});
 
